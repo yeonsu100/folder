4910804e6229fd085279da96c0ab6093c39a94ff
Winnie <yeonsu100@gmail.com>
2020-01-13 12:50:36 +0900

    improve [UsersDao]

--- Step05_Login_/src/test/users/dao/UsersDao.java
+++ Step05_Login_/src/test/users/dao/UsersDao.java
@@ -17,6 +17,69 @@ public class UsersDao {
 		return dao;
 	}
 	
+	// 인자로 전달되는 아이디에 해당하는 가입정보를 리턴해주는 메소드
+	public UsersDto getData(String id) {
+		UsersDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "SELECT pwd, email, regdate"
+					+ " FROM users WHERE id=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				dto=new UsersDto();
+				dto.setId(id);
+				dto.setPwd(rs.getString("pwd"));
+				dto.setEmail(rs.getString("email"));
+				dto.setRegdate(rs.getString("regdate"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)rs.close();
+				if (pstmt != null)pstmt.close();
+				//connection pool 에 반납하기 
+				if (conn != null)conn.close();
+			} catch (Exception e) {}
+		}
+		return dto;
+	}
+	
+	// 인자로 전달되는 아이디가 존재하는지 여부를 리턴하는 메소드
+	public boolean isExist(String inputId) {
+		boolean isExist=false;			// 아이디가 이미 존재하는지 여부
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "SELECT id FROM users WHERE id=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 
+			pstmt.setString(1, inputId);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				isExist=true;		// 이미 존재하는 아이디이므로...
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)rs.close();
+				if (pstmt != null)pstmt.close();
+				// connection pool 에 반납하기 
+				if (conn != null)conn.close();
+			} catch (Exception e) {}
+		}
+		return isExist;			// 아이디가 이미 존재하는지 여부를 리턴해준다.
+	}
+	
 	// 인자로 전달되는 UsersDto 에 담긴 정보가 유효한 정보인지 여부를 리턴해주는 메소드
 	public boolean isValid(UsersDto dto) {
 		boolean isValid=false;
@@ -33,7 +96,7 @@ public class UsersDao {
 			pstmt.setString(2, dto.getPwd());
 			rs = pstmt.executeQuery();
 			while (rs.next()) {//select 된 row 가 있으면
-				//아이디, 비밀번호가 일치함으로 isValid=true 로 바꿔준다.
+				// 아이디, 비밀번호가 일치함으로 isValid=true 로 바꿔준다.
 				isValid=true;
 			}	
 		} catch (Exception e) {
@@ -44,7 +107,7 @@ public class UsersDao {
 					rs.close();
 				if (pstmt != null)
 					pstmt.close();
-				//connection pool 에 반납하기 
+				// connection pool 에 반납하기 
 				if (conn != null)
 					conn.close();
 			} catch (Exception e) {
@@ -74,10 +137,8 @@ public class UsersDao {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
 			} catch (Exception e) {}
 		}
 		if (flag > 0) {
