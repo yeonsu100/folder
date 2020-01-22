101836ba2598ba05f12ee701a05da24846950e9c
Winnie <yeonsu100@gmail.com>
2020-01-22 14:43:56 +0900

    [MyPage] - add 'DbcpBean', 'web.xml'

---
+++ MyPage/src/test/util/DbcpBean.java
@@ -0,0 +1,30 @@
+package test.util;
+
+import java.sql.Connection;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DbcpBean {
+	// 필드
+	private Connection conn;
+	// 생성자
+	public DbcpBean() {
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			// Connection pool에서 Connection 객체 하나 가져오기!
+			// 따라서 Connection 객체가 필요하면 new DbcpBean().getConn(); 하고 불러오면 된다.
+			conn = ds.getConnection();
+			System.out.println("Successfully get a Connection!");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Connection 객체를 리턴해주는 메소드
+	public Connection getConn() {
+		return conn;
+	}
+}
